947e7c8e4f5473c75570ec8a411887de2b521384
Erick G <erick2599@gmail.com>
2022-08-07 12:12:32 -0600

    Creando lista de productos a HTML usando JS a partir de un array

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail')
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -16,7 +19,6 @@ function toggleDesktopMenu() {
     }
     desktopMenu.classList.toggle('inactive');
 }
-
 function toggleMobileMenu(){
     const isAsideClosed=aside.classList.contains('inactive');
     if(!isAsideClosed){
@@ -24,7 +26,6 @@ function toggleMobileMenu(){
     }
     mobileMenu.classList.toggle('inactive');
 }
-
 function toggleCarritoAside(){
     const isMobileMenuClosed=mobileMenu.classList.contains('inactive');
     if(!isMobileMenuClosed){
@@ -32,3 +33,98 @@ function toggleCarritoAside(){
     }
     aside.classList.toggle('inactive');
 }
+
+const productList = [];
+/*Estos son todos los productos*/
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'GeForce GTX 3070ti',
+    price: 640,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Asus 3070 ROG',
+    price: 700,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Ryzen 5800x',
+    price: 400,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'GeForce GTX 3070ti',
+    price: 640,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Asus 3070 ROG',
+    price: 700,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Ryzen 5800x',
+    price: 400,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+
+/*Son para iterar*/
+for ( product of productList){
+    console.log(product.name)
+};
+
+for ( product in productList){
+    console.log(product)
+};
+
+/*Con este codigo se pueden crear los objetos en HTML usando JS*/
+//Se puede usar una funcion
+
+function renderProducts(arr) {
+    for(product of arr){
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    //product = {name,price.image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src',product.image);
+    
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+};
+};
+
+renderProducts(productList);
